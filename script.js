7319a0972de7d14757cc32f5ef646c779508a64f
Giovanni <giovannibsf@gmail.com>
2023-06-03 18:02:39 -0300

    fazendo pagina Dados

---
+++ script.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal('.text1', { duration: 1000})
+sr.reveal('.text2', { duration: 1000})
+sr.reveal('.text3', { duration: 1000})
+sr.reveal('.graf-um', { 
+    rotate: {x: 100, y: 0, z: 0},
+    duration: 2000})
+sr.reveal('.imag-1', { 
+    rotate: {x: 100, y: 0, z: 0},
+    duration: 2000})
+sr.reveal('.graf-dois', { 
+    rotate: {x: 100, y: 0, z: 0},
+    duration: 2000})
+
+
+sr.reveal('#item-um', { 
+    rotate: {x: 0, y: 30, z: 0},
+    duration: 1000
+})
+sr.reveal('#item-dois', { 
+    rotate: {x: 0, y: 30, z: 0},
+    duration: 3000
+})
+sr.reveal('#item-tres', { 
+    rotate: {x: 0, y: 30, z: 0},
+    duration: 4000
+})
