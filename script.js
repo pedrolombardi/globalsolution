fa40e4d12a24f9f01a5c7ffae1bf9b61669b9812
Giovanni <giovannibsf@gmail.com>
2023-06-06 16:50:51 -0300

    fiz a pagina de contato e fazendo a index

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.sr = ScrollReveal({ reset: true })
 
-sr.reveal('.text1', { duration: 1000})
-sr.reveal('.text2', { duration: 1000})
-sr.reveal('.text3', { duration: 1000})
+sr.reveal('.text1', { scale: 1.5 })
+sr.reveal('.text2', { distance: '50px'})
+sr.reveal('.text3', { distance: '50px'})
 // sr.reveal('.graf-um', { 
 //     rotate: {x: 100, y: 0, z: 0},
 //     duration: 2000})
@@ -13,6 +13,9 @@ sr.reveal('.image-food', {
     rotate: {x: 100, y: 0, z: 0},
     duration: 2000})
 
+sr.reveal('.img-main-one', {
+    distance: '50px'
+})
 
 sr.reveal('#item-dois', { 
     rotate: {x: 0, y: 30, z: 0},
